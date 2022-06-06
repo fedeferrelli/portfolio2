264e59ca272d58bfaa620e5a40e7116cb98d9fb9
Fede <ferrelli.mazza@gmail.com>
2022-06-06 18:48:02 -0300

    Restyle showprojects at every width screen

--- src/Components/Portfolio/ShowProject.jsx
+++ src/Components/Portfolio/ShowProject.jsx
@@ -30,13 +30,26 @@ function ShowProject({ project, index, language='esp' }) {
 
         <div >
         <div className=" sm:w-full bg-secondary/5  m-auto rounded border border-secondary/20 drop-shadow-[0_0px_15px_rgba(0,0,0,0.5)] h-full">
-          <h1 className="text-center text-2xl sm:text-2xl font-bold my-3 flex flex-row justify-center items-center text-secondary">
+          <h1 className="text-center text-2xl sm:text-2xl font-bold mt-3 flex flex-row justify-center items-center text-secondary">
             <span className="w-6 h-6 mr-1 rounded-full text-primary  bg-terciary  text-sm  text-center items-center flex flex-row justify-center">
               {reindex}
             </span>
             {name}
           </h1>
-          <div className=" sm:hidden w-40 m-auto text-center text-terciary/50 rounded-full px-2 border border-terciary" onClick={()=> setShowDescription (!showDescription)}>{!showDescription ? "ver descripción" : "ver imagen"}</div>
+          <div className="hidden sm:flex flex-wrap justify-center sm:py-2 h-auto item-center w-full">
+                {stack.map((item) => (
+                  <div
+                    key={Math.random()
+                      .toString(36)
+                      .replace(/[^a-z]+/g, "")
+                      .substr(0, 5)}
+                    className="py-0.5 px-3 text-xs rounded-full border border-terciary/50 m-1.5 text-terciary/50 "
+                  >
+                    {item}
+                  </div>
+                ))}
+              </div>
+          <div className=" sm:hidden w-40 m-auto text-center text-terciary/50 rounded-full px-2 py-1 mb-2 border border-terciary" onClick={()=> setShowDescription (!showDescription)}>{!showDescription ? "ver descripción" : "ver imagen"}</div>
 
           <section className="flex flex-col sm:flex-row ">
 
@@ -50,20 +63,20 @@ function ShowProject({ project, index, language='esp' }) {
             <img
                 src={image}
                 alt={image}
-                className="w-[350px] sm:w-[450px] h-[160px] sm:h-[206px] m-auto"
+                className="w-[450px] h-[206px] m-auto"
               />
       
         </div>      
             :
-            <div className="w-full  flex flex-col justify-evenly items-center py-2 min-h-[420px] m-auto ">
+            <div className="w-full  flex flex-col justify-evenly items-center py-2 gap-4 m-auto">
 
 
-            <p className=" px-4 py-2 font-mono sm:px-8 text-center flex text-secondary max-w-prose">
+            <p className=" px-4 py-1 font-mono sm:px-8 text-center flex text-secondary max-w-prose">
                            {description_esp}
                          </p>
            
            
-                         <div className="flex flex-wrap justify-center  h-auto item-center w-full">
+                         <div className="flex flex-wrap justify-center h-auto item-center w-full">
                            {stack.map((item) => (
                              <div
                                key={Math.random()
@@ -81,69 +94,25 @@ function ShowProject({ project, index, language='esp' }) {
             
           </div>
 
-          <div className="w-full flex flex-col justify-evenly items-center py-2 ">
+          <div className="w-full flex flex-col justify-evenly items-center py-0 ">
 
-
- <p className=" px-4 py-2 font-mono sm:px-8 text-center hidden sm:flex text-secondary">
+<div className="px-4 py-2 font-mono sm:px-8 text-center hidden sm:flex justify-center items-center text-secondary">
+ <p className="">
                 {description_esp}
               </p>
-
-
-              <div className="hidden sm:flex flex-wrap justify-center  h-auto item-center w-full">
-                {stack.map((item) => (
-                  <div
-                    key={Math.random()
-                      .toString(36)
-                      .replace(/[^a-z]+/g, "")
-                      .substr(0, 5)}
-                    className="py-0.5 px-3 text-sm rounded-full border border-terciary m-1.5 text-terciary "
-                  >
-                    {item}
-                  </div>
-                ))}
               </div>
 
-{/* 
-              <div className="w-full px-4 py-2 sm:py-0 flex flex-col gap-2 sm:gap-4 sm:flex-row justify-evenly  items-center">
-
-            <a
-              href={link ? link : apk}
-              rel="noreferrer"
-              target="_blank"
-              className="sm:bg-terciary overflow-hidden  rounded-lg text-terciary sm:text-primary font-sans
-         sm:hover:terciary/50 border border-terciary ease-in-out duration-300 w-full"
-            >
-              {" "}
-             <button className="uppercase text-center sm:px-6 py-2 w-full "> {link ? "site" : "apk"}</button>
-            </a>
-            
-            <a
-              href={code}
-              rel="noreferrer"
-              target="_blank"
-              className="sm:bg-terciary overflow-hidden  rounded-lg text-terciary sm:text-primary font-sans
-         sm:hover:terciary/50 border border-terciary ease-in-out duration-300 w-full"
-            >
-              {" "}
-             <button className="uppercase text-center sm:px-6 py-2 w-full ">Code</button>
-            </a>
-
-
-            </div> */}
-          </div>          
-          </section>
-          
-          <div className="w-full px-4 py-2 sm:py-10 flex flex-col gap-2 sm:gap-4 sm:flex-row justify-evenly  items-center">
+              <div className="w-full px-4 py-2 sm:py-2 flex flex-col gap-2 sm:gap-4 sm:flex-row justify-evenly  items-center">
 
 <a
   href={link ? link : apk}
   rel="noreferrer"
   target="_blank"
   className="sm:bg-terciary overflow-hidden  rounded-lg text-terciary sm:text-primary font-sans
-sm:hover:terciary/50 border border-terciary ease-in-out duration-300 w-full sm:w-1/3"
+sm:hover:terciary/50 border border-terciary ease-in-out duration-300 w-full sm:w-1/2"
 >
   {" "}
- <button className="uppercase text-center sm:px-6 py-2 w-full "> {link ? "site" : "apk"}</button>
+ <button className="uppercase text-center py-2 sm:py-1 w-full "> {link ? "site" : "apk"}</button>
 </a>
 
 <a
@@ -151,14 +120,23 @@ sm:hover:terciary/50 border border-terciary ease-in-out duration-300 w-full sm:w
   rel="noreferrer"
   target="_blank"
   className="sm:bg-terciary overflow-hidden  rounded-lg text-terciary sm:text-primary font-sans
-sm:hover:terciary/50 border border-terciary ease-in-out duration-300 w-full sm:w-1/3"
+sm:hover:terciary/50 border border-terciary ease-in-out duration-300 w-full sm:w-1/2"
 >
   {" "}
- <button className="uppercase text-center sm:px-6 py-2 w-full ">Code</button>
+ <button className="uppercase text-center py-2 sm:py-1 w-full ">Code</button>
 </a>
 
 
 </div>
+
+
+          </div>   
+                 
+          </section>
+          
+ 
+
+              
         </div>
         </div>
       </Fade>
